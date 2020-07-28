67fc5d402f2b33d2fd048b2ec57c36c66b0b5e6c
225junior <225juniorbrindou@gmail.com>
2020-07-28 17:30:08 +0000

    domaine -mvc

---
+++ app/Http/Controllers/DomaineController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domaine;
+use Illuminate\Http\Request;
+
+class DomaineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Domaine  $domaine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Domaine $domaine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Domaine  $domaine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Domaine $domaine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Domaine  $domaine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Domaine $domaine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Domaine  $domaine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Domaine $domaine)
+    {
+        //
+    }
+}
