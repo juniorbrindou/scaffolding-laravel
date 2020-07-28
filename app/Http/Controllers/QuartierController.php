f342b7ad7108c73b8f3cdd4e71e6eadc4c2578fc
225junior <225juniorbrindou@gmail.com>
2020-07-28 17:20:59 +0000

    Quartier -mvc

---
+++ app/Http/Controllers/QuartierController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quartier;
+use Illuminate\Http\Request;
+
+class QuartierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Quartier  $quartier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quartier $quartier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Quartier  $quartier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Quartier $quartier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Quartier  $quartier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Quartier $quartier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Quartier  $quartier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Quartier $quartier)
+    {
+        //
+    }
+}
